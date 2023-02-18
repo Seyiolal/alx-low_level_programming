1766cb2a7699f8ff553596b17635b3073cbc23e1
seyiolal <seyiolal@yahoo.com>
2023-02-17 16:40:25 -0800

    pos n neg

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -11,7 +11,8 @@
  */
 
 int main(void)
-{ int n;
+{ 
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -23,3 +24,4 @@ int main(void)
 	else
 		printf("%d is negative\n", n);
 	return (0);
+}
