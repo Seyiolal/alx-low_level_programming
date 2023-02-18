cb29e4e0f36a7fea8c952715fcf35b1ed9fd07ae
seyiolal <seyiolal@yahoo.com>
2023-02-17 16:19:04 -0800

    var,if,else,while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwaya 0 (success)
+ */
+int main(void)
+{
+        char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
