74487e8a79d471eaf9882ef3d9e07c7ad8d348e1
seyiolal <seyiolal@yahoo.com>
2023-02-24 02:48:44 -0800

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,23 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
-	}
-	int i, j;
+	} else
+	{
+		int i, j;
+
 
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
 		{
+			_putchar(' ');
+		}
+
+		for (j = 1; j <= i; j++)
+		{
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
